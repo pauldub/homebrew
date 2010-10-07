538ccb6fa02c7eef0d715b79b34bd8929c8654e3
Adam Vandenberg <flangy@gmail.com>
2010-10-06 18:27:49 -0700

    wip

--- Library/Formula/reia.rb
+++ Library/Formula/reia.rb
@@ -1,23 +1,27 @@
 require 'formula'
 
 class Reia < Formula
-  version 'HEAD' #Reia only has Git-repository, no stable versions released and
-                 #Reia cannot be installed into a directory with dots in it like 0.1 (at this time)
-  url 'git://github.com/tarcieri/reia.git'
+  head 'git://github.com/tarcieri/reia.git'
   homepage 'http://reia-lang.org/'
-  md5 '' #Git-repository only, no stable version and no MD5 available
 
   depends_on 'erlang'
 
   def install
-    system 'rake'                                           #build and test Reia
-    %w(bin/ire bin/reia).each do |f|                        #Change some paths in scripts
+    system 'rake'
+
+    # Change some paths in scripts
+    %w(bin/ire bin/reia).each do |f|
       inreplace f, /^(EXTRA_PATHS=).*$/, "\\1\"-pz #{prefix}/ebin\""
       inreplace f, /^(export REIA_HOME=)\.$/, "\\1#{prefix}"
     end
-    libexec.install Dir['lib']                              #move contents of lib to libexec
-    prefix.install Dir['*']                                 #install the rest
-    lib.mkpath                                              #create lib-folder
-    Dir["#{libexec}/lib/*"].each { |f| ln_s f, lib }        #link contents of libexec/lib to lib
+
+    # move contents of lib to libexec
+    libexec.install 'lib'
+    # install the rest
+    prefix.install Dir['*']
+
+    # link contents of libexec/lib to lib
+    lib.mkpath
+    Dir["#{libexec}/lib/*"].each { |f| ln_s f, lib }
   end
 end
