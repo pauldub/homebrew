f5621570fe17898bddde16b362c15fd3f6f6f09e
Adam Vandenberg <flangy@gmail.com>
2010-10-22 09:51:54 -0700

    wip

--- Library/Formula/couchbeam.rb
+++ Library/Formula/couchbeam.rb
@@ -2,16 +2,16 @@ require 'formula'
 
 class Couchbeam <Formula
   homepage 'http://benoitc.github.com/couchbeam/'
-
-  url 'http://github.com/benoitc/couchbeam/tarball/0.4.2'
-  md5 '72dbc30a1ad9a1dd2de22d2f36d910a6'
+  url 'http://github.com/benoitc/couchbeam/tarball/0.5.1'
+  md5 'a11ce498d42f348fdeef90d0286de4aa'
 
   depends_on 'erlang'
 
   def install
-    erlang = Formulary.read("erlang").new("erlang")
+    ENV.j1 # Can't get multiple deps at once
+    erlang_lib = Formula.factory("erlang").lib
     system "make"
-    system "cp -R . #{prefix}"
-    system "ln -s #{prefix} #{erlang.lib}/erlang/lib/couchbeam-#{version}"
+    libexec.install Dir['*']
+    system "ln -s #{libexec} #{erlang_lib}/erlang/lib/couchbeam-#{version}"
   end
 end
