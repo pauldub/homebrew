fe9f34ac1d6cf87a74e3dbbb85c7e048a9d59ce4
Adam Vandenberg <flangy@gmail.com>
2010-10-25 13:16:24 -0700

    Move prune into HomebrewPrefix

--- Library/Homebrew/commands/prune.rb
+++ Library/Homebrew/commands/prune.rb
@@ -2,21 +2,7 @@ def brew_prune
   $n=0
   $d=0
 
-  dirs=Array.new
-  paths=%w[bin sbin etc lib include share].collect {|d| HOMEBREW_PREFIX+d}
-
-  paths.each do |path|
-    path.find do |p|
-      p.extend ObserverPathnameExtension
-      if p.symlink?
-        p.unlink unless p.resolved_path_exists?
-      elsif p.directory?
-        dirs << p
-      end
-    end
-  end
-
-  dirs.sort.reverse_each {|d| d.rmdir_if_possible}
+  HOMEBREW_PREFIX.prune
 
   if $n == 0 and $d == 0
     puts "Nothing pruned" if ARGV.verbose?
