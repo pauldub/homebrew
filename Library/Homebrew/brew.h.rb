3f17a8a14dc03cb8983adeca0d7936f01d1491ad
Adam Vandenberg <flangy@gmail.com>
2010-10-26 08:47:35 -0700

    Add compilers.rb and move code around

--- Library/Homebrew/brew.h.rb
+++ Library/Homebrew/brew.h.rb
@@ -87,45 +87,6 @@ def search_brews text
   end
 end
 
-def gcc_42_build
-  `/usr/bin/gcc-4.2 -v 2>&1` =~ /build (\d{4,})/
-  if $1
-    $1.to_i 
-  elsif system "/usr/bin/which gcc"
-    # Xcode 3.0 didn't come with gcc-4.2
-    # We can't change the above regex to use gcc because the version numbers
-    # are different and thus, not useful.
-    # FIXME I bet you 20 quid this causes a side effect — magic values tend to
-    401
-  else
-    nil
-  end
-end
-alias :gcc_build :gcc_42_build # For compatibility
-
-def gcc_40_build
-  `/usr/bin/gcc-4.0 -v 2>&1` =~ /build (\d{4,})/
-  if $1
-    $1.to_i 
-  else
-    nil
-  end
-end
-
-def llvm_build
-  if MACOS_VERSION >= 10.6
-    xcode_path = `/usr/bin/xcode-select -print-path`.chomp
-    return nil if xcode_path.empty?
-    `#{xcode_path}/usr/bin/llvm-gcc -v 2>&1` =~ /LLVM build (\d{4,})/
-    $1.to_i
-  end
-end
-
-def xcode_version
-  `xcodebuild -version 2>&1` =~ /Xcode (\d(\.\d)*)/
-  return $1 ? $1 : nil
-end
-
 def _compiler_recommendation build, recommended
   message = (!build.nil? && build < recommended) ? "(#{recommended} or newer recommended)" : ""
   return build, message
