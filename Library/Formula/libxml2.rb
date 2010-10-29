7d315f8a1ce1b40dc753dfab5f8a871468017751
Adam Vandenberg <flangy@gmail.com>
2010-10-29 09:17:52 -0700

    wip

--- Library/Formula/libxml2.rb
+++ Library/Formula/libxml2.rb
@@ -1,5 +1,7 @@
 require 'formula'
 
+def build_python?; ARGV.include? "--python"; end
+
 class Libxml2 <Formula
   url 'ftp://xmlsoft.org/libxml2/libxml2-2.7.7.tar.gz'
   homepage 'http://xmlsoft.org'
@@ -7,9 +9,20 @@ class Libxml2 <Formula
 
   keg_only :provided_by_osx
 
+  def options
+    [["--python", "Also build and install Python bindings."]]
+  end
+
   def install
     fails_with_llvm "Undefined symbols when linking", :build => "2326"
-    system "./configure", "--prefix=#{prefix}", "--disable-dependency-tracking"
+
+    args = ["--disable-dependency-tracking", "--prefix=#{prefix}"]
+
+    if build_python?
+      args << "--with-python=`python-config --prefix`"
+    end
+
+    system "./configure", *args
     system "make"
     ENV.j1
     system "make install"
