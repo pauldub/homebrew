80051020325ebbf5c59eab6733bba6613c13205d
Adam Vandenberg <flangy@gmail.com>
2010-11-13 20:49:09 -0800

    Move options to cmds

---
+++ Library/Homebrew/cmd/options.rb
@@ -0,0 +1,13 @@
+module Homebrew extend self
+  def options
+    ARGV.formulae.each do |f|
+      f.options rescue next
+      puts f.name
+      f.options.each do |o|
+        puts o[0]
+        puts "\t"+o[1]
+      end
+      puts
+    end
+  end
+end
